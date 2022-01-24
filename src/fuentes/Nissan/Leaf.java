846c452fbe724852cb4d2f2d165b12afeeb5deed
oundurraga <oscarua@gmail.com>
2022-01-24 20:02:45 -0300

    Leaf

---
+++ src/fuentes/Nissan/Leaf.java
@@ -0,0 +1,55 @@
+package fuentes.Nissan;
+
+public class Leaf extends fuentes.AutoElectrico {
+	private String modelo;
+	private int puertas;
+	private int potencia;
+	private int anio;
+	private int precio;
+
+	public Leaf() {
+		
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	
+}
--- src/fuentes/Principal.java
+++ src/fuentes/Principal.java
@@ -1,5 +1,7 @@
 package fuentes;
 
+import fuentes.Nissan.Leaf;
+
 public class Principal {
 	public static void main(String[] args) {
 		Vehiculo autoelectrico = new AutoElectrico();
@@ -10,6 +12,17 @@ public class Principal {
 		
 		System.out.println("El rendimiento del auto electrico es: " + autoelectrico.getRendimiento());
 		System.out.println("El rendimiento del auto diesel es: " + autodiesel.getRendimiento());
+		
+		Leaf auto1 = new Leaf();
+		auto1.setRendimiento(35);
+		auto1.setAnio(2022);
+		auto1.setModelo("Leaf 62 kWh e+ Acenta - 5-puertas");
+		auto1.setPrecio(28400);
+		auto1.setPuertas(5);
+		auto1.setPotencia(218);
+		
+		System.out.println("El " + auto1.getModelo() + " tiene un rendimiento de "+  auto1.getRendimiento());
+
 		
 	}
 }
